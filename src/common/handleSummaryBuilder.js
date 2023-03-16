49e64a2a03747c3c0deb52bc8e8c2c18533df127
anttorre <antonio.torre@nttdata.com>
2023-03-16 14:05:53 +0100

    printing result directory

--- src/common/handleSummaryBuilder.js
+++ src/common/handleSummaryBuilder.js
@@ -10,6 +10,8 @@ export default (outputFile) => (data) => {
     console.log(
         `TEST DETAILS: [Time to complete test: ${data.state.testRunDurationMs} ms, Environment target: ${__ENV.TARGET_ENV}, Scenario test type: ${__ENV.SCENARIO_TYPE_ENV}, Number of VUs: ${__ENV.VIRTUAL_USERS_ENV}, Request processed: ${data.metrics.http_reqs.values.count}, Request OK: ${data.metrics.http_req_failed.values.fails}, ERRORS: ${data.metrics.http_req_failed.values.passes}]`
     )
+    console.log(`Exporting results into: ${outputDir}/results/${outputFile}`)
+
     if (__ENV.SCENARIO_TYPE_ENV == 'rampingArrivalRate') {
         let stringRamping = 'Ramping iterations for stage : { '
         for (let i = 0; i < customStages.length - 1; i++) {
