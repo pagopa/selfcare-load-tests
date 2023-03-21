94315a327b367c6b3e17133c3278d33123466ac8
anttorre <antonio.torre@nttdata.com>
2023-03-21 11:51:02 +0100

    test

--- run.sh
+++ run.sh
@@ -40,7 +40,7 @@ if [[ -n "$INFLUXDB_URL" ]]; then
   for url in ${INFLUXDB_URL//;/ } ; do
     CURL_EXIT_CODE=
     wget --timeout 1 --tries 1 $url || CURL_EXIT_CODE=$?
-    echo $url -> $CURL_EXIT_CODE
+    echo "$url -> $CURL_EXIT_CODE"
     if [[ "$CURL_EXIT_CODE" == "8" ]]; then
       INFLUXDB_CONFIG="--out influxdb=$url"
     fi
